338865b1842062ede07aaf4912fef35d16cbc829
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-06 15:01:17 -0500

    Setting up the initial files

---
+++ data/migrations/20190606145412_add_dish_table.js
@@ -0,0 +1,7 @@
+exports.up = function(knex, Promise) {
+  //   return knex.shcema.createTable("dish", function(tbl) {
+  //     tbl.increments();
+  //   });
+};
+
+exports.down = function(knex, Promise) {};
